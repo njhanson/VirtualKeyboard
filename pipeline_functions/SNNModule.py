6f7e50ae5e3d3afcf784ed0a7a6deb127ff4f9d5
MacFadyenK <khmacfadyen@oakland.edu>
2026-03-11 16:58:11 -0400

    initialized weights and biases in normal distribution, changed how membrane potential is initialized, separately modify betas and thresholds for each layer

--- pipeline_functions/SNNModule.py
+++ pipeline_functions/SNNModule.py
@@ -1,10 +1,11 @@
 # imports
 import snntorch as snn
+from snntorch import utils
 
 import torch
 import torch.nn as nn
 
-def createSNN(dim_inputs, hidden_layer, num_outputs=2, beta=0.9):
+def createSNN(dim_inputs, hidden_layer, num_outputs=2, betas=[0.9, 0.9], thresholds=[1, 1]):
     """
     Function wrapper that initiates a fully connected 3 layer SNN model
     
@@ -12,24 +13,31 @@ def createSNN(dim_inputs, hidden_layer, num_outputs=2, beta=0.9):
     - dim_inputs: dimension size of the input features (flattened)
     - hidden_layer: number of neurons in the hidden layer of the model
     - num_outputs: number of output neurons/classes in the model. Default = 2
-    - beta: decay constant for the LIF neurons. Default = 0.9
+    - betas: decay constant for each LIF neuron layer as an array. Default = [0.9, 0.9]
+    - thresholds: array of membrane potential thresholds for the neurons in each layer to produce a spike, Default = [1, 1]
 
     Returns: 
     - A fully connected 3 layer spiking neural network with the specified parameters
     """
-    return fcSNN(dim_inputs=dim_inputs, hidden_layer=hidden_layer, num_outputs=num_outputs, beta=beta)
+    return fcSNN(dim_inputs=dim_inputs, hidden_layer=hidden_layer, num_outputs=num_outputs, betas=betas, thresholds=thresholds)
 
 
 class fcSNN(nn.Module):
-    def __init__(self, dim_inputs, hidden_layer, num_outputs, beta):
+    def __init__(self, dim_inputs, hidden_layer, num_outputs, betas, thresholds):
         super().__init__()
 
         # initializes lif and linear layers for the SNN
-        # uses one beta for both lif neuron layers, can be updated if we need more control
         self.fc1 = nn.Linear(dim_inputs, hidden_layer)
-        self.lif1 = snn.Leaky(beta=beta)
+        self.lif1 = snn.Leaky(beta=betas[0], threshold=thresholds[0], init_hidden=True)
         self.fc2 = nn.Linear(hidden_layer, num_outputs)
-        self.lif2 = snn.Leaky(beta=beta)
+        self.lif2 = snn.Leaky(beta=betas[1], threshold=thresholds[1], init_hidden=True, output=True)
+
+        # initializes fully connected layer weights and biases in a small normal distribution.
+        # weights are skewed positive to encourage positive membrane potentials and to produce output spikes
+        nn.init.normal_(self.fc1.weight, mean=0.015, std=0.007)
+        nn.init.normal_(self.fc2.weight, mean=0.01, std=0.02)
+        nn.init.normal_(self.fc1.bias, mean=0.0, std=0.001)
+        nn.init.normal_(self.fc2.bias, mean=0.0, std=0.01)
     
     def forward(self, x, batch_first=False):
         """
@@ -49,9 +57,7 @@ class fcSNN(nn.Module):
             x = x.transpose(0, 1)
         x = torch.flatten(x, start_dim=2)
 
-        #initializing hidden states of lif neurons
-        mem1 = self.lif1.init_leaky()
-        mem2 = self.lif2.init_leaky()
+        utils.reset(self)
 
         # record final layer
         spk2_rec = []
@@ -60,10 +66,10 @@ class fcSNN(nn.Module):
         # through the time steps of the data
         for step in range(x.size(0)): # number of time steps in x
             cur1 = self.fc1(x[step])
-            spk1, mem1 = self.lif1(cur1, mem1)
+            spk1 = self.lif1(cur1)
 
             cur2 = self.fc2(spk1)
-            spk2, mem2 = self.lif2(cur2, mem2)
+            spk2, mem2 = self.lif2(cur2)
 
             spk2_rec.append(spk2)
             mem2_rec.append(mem2)
